d345a922a1159d85b232a99459f8242f53d46826
solalcaraz <alcarazmsol@gmail.com>
2025-10-10 17:50:47 -0300

    borro dos archivos q agregue al pepe

--- src/main/kotlin/AQPApplication.kt
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class AQPApplication
-
-fun main(args: Array<String>) {
-    runApplication<AQPApplication>(*args)
-}
