9c9fa6230c86ed7f1e02c03b7105588183818f35
carlasolrocca <carlasolrocca@gmail.com>
2025-10-12 18:27:12 -0300

    Primeros metodos http para PedidoController

---
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/PedidoController.kt
@@ -0,0 +1,21 @@
+package ar.edu.unsam.algo3.ar.edu.unsam.algo3.controller
+
+import ar.edu.unsam.algo3.service.PedidoService
+import org.springframework.web.bind.annotation.*
+
+
+@RestController
+@CrossOrigin("*")
+class PedidoController(val pedidoService : PedidoService) {
+    @GetMapping("/pedidos")
+    fun listarPedidos() = pedidoService.getAll()
+
+    @GetMapping("/pedidos/{estado}")
+    fun listarPedidosPorEstado(@PathVariable estado : String) = pedidoService.getByEstado(estado)
+
+    @GetMapping("/pedidos/{id}")
+    fun listarPedidosPorEstado(@PathVariable id : Int) = pedidoService.getById(id)
+
+    @PostMapping("/pedidos")
+    fun actualizarEstado() = pedidoService.actualizarEstado()   //No recibe nada porque siempre pasa al siguiente estado
+}
