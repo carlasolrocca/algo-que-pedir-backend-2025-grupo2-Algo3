045f3532211e2b963273f55cbb6feec0ae018e53
carlasolrocca <carlasolrocca@gmail.com>
2025-10-12 19:05:48 -0300

    Fix package line en PedidoService + functions definidas

---
+++ src/main/kotlin/ar/edu/unsam/algo3/service/PedidoService.kt
@@ -0,0 +1,11 @@
+package ar.edu.unsam.algo3.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class PedidoService {
+    fun getAll(){}
+    fun getByEstado(estado : String){}
+    fun getById(id: Int){}
+    fun actualizarEstado(){}
+}
