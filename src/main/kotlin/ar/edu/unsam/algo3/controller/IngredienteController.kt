b2320cc66a525566f3b59d5720013e647fdaade7
Joaquín Navarro <joanavarro23@hotmail.com>
2025-10-13 02:37:14 -0300

    se agrega put para actualizar en controller

--- src/main/kotlin/ar/edu/unsam/algo3/controller/IngredienteController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/IngredienteController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,6 +18,11 @@ class IngredienteController(val ingredienteService: IngredienteService) {
 
     @GetMapping("/ingrediente/{id}")
     fun ingredientePorId(@PathVariable id: Int): Ingrediente = ingredienteService.getById(id)
+
+    @PutMapping("/ingrediente/{id}")
+    fun actualizarIngrediente(@PathVariable id: Int, @RequestBody ingredienteBody: Ingrediente): Ingrediente {
+        return ingredienteService.update(id, ingredienteBody)
+    }
 
     @PostMapping("/ingrediente")
     fun crearIngrediente(@RequestBody ingredienteBody: Ingrediente): Ingrediente =
