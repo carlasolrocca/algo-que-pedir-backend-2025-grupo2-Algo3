549fce3ee6041331f3ba34ff700ef7a5277d2d2a
carlasolrocca <carlasolrocca@gmail.com>
2025-10-12 22:44:11 -0300

    Creo el DTO para listar todos los Pedidos

---
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/ListadoPedidosDTO.kt
@@ -0,0 +1,13 @@
+package ar.edu.unsam.algo3.dto
+
+class ListadoPedidosDTO {
+    var id: Int ? = null
+    var clienteNombre: String = ""
+    var clienteUsername : String = ""
+    var hora : String = ""
+    var items : Int = 0
+    var precioTotal : Double = 0.0
+    var estado : String = ""
+    var direccion : String = ""
+    var medioDePago : String = ""
+}
