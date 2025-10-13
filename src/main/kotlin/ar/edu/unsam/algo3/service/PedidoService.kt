1de5cea3f397e2ac272c047ce5dc7eff22186c81
carlasolrocca <carlasolrocca@gmail.com>
2025-10-13 16:50:05 -0300

    PedidoService v1

--- src/main/kotlin/ar/edu/unsam/algo3/service/PedidoService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/PedidoService.kt
@@ -1,5 +1,8 @@
 package ar.edu.unsam.algo3.service
 
+import ar.edu.unsam.algo3.Pedido
+import ar.edu.unsam.algo3.dto.PedidoDTO
+import ar.edu.unsam.algo3.dto.toDTO
 import org.springframework.stereotype.Service
 import ar.edu.unsam.algo3.repositorios.PedidoRepositorio
 
@@ -8,11 +11,11 @@ import ar.edu.unsam.algo3.repositorios.PedidoRepositorio
 class PedidoService(
     private val pedidoRepo : PedidoRepositorio
 ) {
-    fun getAll() = pedidoRepo.findAll()
+    fun getAll() : List<PedidoDTO> = pedidoRepo.findAll().map {it.toDTO()}
 
-    fun getByEstado(estado : String) = pedidoRepo.search(estado)
+    fun getByEstado(estado : String) : List<Pedido> = pedidoRepo.search(estado)
 
-    fun getById(id: Int) = pedidoRepo.getById(id)
+    fun getById(id: Int) : Pedido = pedidoRepo.getById(id)
 
     fun actualizarEstado(){}
 }
