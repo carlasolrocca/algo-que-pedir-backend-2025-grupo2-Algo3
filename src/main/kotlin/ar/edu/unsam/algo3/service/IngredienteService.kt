2eb595984b8fd44bf75b59427d3704a31cb2f2cd
Joaquín Navarro <joanavarro23@hotmail.com>
2025-10-13 02:37:57 -0300

    metodo update en service

--- src/main/kotlin/ar/edu/unsam/algo3/service/IngredienteService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/IngredienteService.kt
@@ -1,5 +1,6 @@
 package ar.edu.unsam.algo3.ar.edu.unsam.algo3.service
 
+import ar.edu.unsam.algo3.ErrorException
 import ar.edu.unsam.algo3.Ingrediente
 import ar.edu.unsam.algo3.repositorios.IngredienteRepositorio
 import org.springframework.stereotype.Service
@@ -12,5 +13,17 @@ class IngredienteService(private val repositorioIngrediente: IngredienteReposito
     fun create(nuevoIngrediente: Ingrediente): Ingrediente {
         repositorioIngrediente.create(nuevoIngrediente)
         return nuevoIngrediente
+    }
+    fun update(id: Int, ingredienteActualizado: Ingrediente): Ingrediente {
+        if (ingredienteActualizado.id == null) {
+            throw ErrorException.BusinessException("Debe proveerse ID del ingrediente a actualizar")
+        }
+        if (ingredienteActualizado.id!! != id) {
+            throw ErrorException.BusinessException("ID en URL distinto del ID en body")
+        }
+        val ingrediente = getById(id)
+        ingrediente.actualizar(ingredienteActualizado)
+        repositorioIngrediente.update(ingrediente)
+        return ingrediente
     }
 }
