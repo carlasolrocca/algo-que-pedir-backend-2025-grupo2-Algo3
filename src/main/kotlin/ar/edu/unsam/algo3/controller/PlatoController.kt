011fd9f45e7c8eb2f10951498c7659463781674d
solalcaraz <alcarazmsol@gmail.com>
2025-10-13 22:01:42 -0300

    devuelvo un DTO para getAll y post

--- src/main/kotlin/ar/edu/unsam/algo3/controller/PlatoController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/PlatoController.kt
@@ -1,6 +1,7 @@
 package ar.edu.unsam.algo3.controller
 
 import ar.edu.unsam.algo3.Plato
+import ar.edu.unsam.algo3.dto.PlatoDTO
 import ar.edu.unsam.algo3.dto.toDTO
 import ar.edu.unsam.algo3.service.PlatoService
 import org.springframework.web.bind.annotation.*
@@ -9,14 +10,15 @@ import org.springframework.web.bind.annotation.*
 @CrossOrigin("*")
 class PlatoController(val platoService: PlatoService) {
     @GetMapping("/plato")
-    fun listarPlatos() = platoService.getAll()
+    fun listarPlatos(): Set<PlatoDTO> =
+        platoService.getAll().map { it.toDTO() }.toMutableSet()
 
     @GetMapping("/plato/{id}")
     fun platoPorId(@PathVariable id: Int) = platoService.getById(id)?.toDTO()
 
     @PostMapping("/plato")
-    fun crearPlato(@RequestBody platoBody: Plato): Plato =
-        platoService.create(platoBody)
+    fun crearPlato(@RequestBody platoBody: Plato): PlatoDTO =
+        platoService.create(platoBody).toDTO()
 
     @PutMapping("/plato/{id}")
     fun actualizarPlato(@PathVariable id: Int, @RequestBody platoBody: Plato): Plato =
