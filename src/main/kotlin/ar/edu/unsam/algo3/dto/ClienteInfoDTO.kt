335f79b71355ae1ac07ba8e127d8bdeae2be024c
carlasolrocca <carlasolrocca@gmail.com>
2025-10-21 14:35:35 -0300

    Elimino acoplamiento direccion en ClienteInfoDTO

--- src/main/kotlin/ar/edu/unsam/algo3/dto/ClienteInfoDTO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/ClienteInfoDTO.kt
@@ -5,12 +5,10 @@ import ar.edu.unsam.algo3.Usuario
 data class ClienteInfoDTO (
     val nombre : String,
     val username : String,
-    val direccion : String
 )
 
 fun Usuario.toDTO() : ClienteInfoDTO =
     ClienteInfoDTO(
         nombre = "${this.nombre} ${this.apellido}".trim(),
         username = this.username,
-        direccion = this.direccion.devolverDireccionCompleta()
     )
