b1c1f6a1a61ea9cd7ea17800c801c99f071da57d
carlasolrocca <carlasolrocca@gmail.com>
2025-10-26 20:31:52 -0300

    Reagrego metodo que devuelve direc completa

--- src/main/kotlin/ar/edu/unsam/algo3/dto/DireccionDTO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/DireccionDTO.kt
@@ -3,16 +3,14 @@ package ar.edu.unsam.algo3.dto
 import ar.edu.unsam.algo3.Direccion
 
 data class DireccionDTO(
-    val calle : String,
-    val altura: Int,
+    val direccion : String,
     val latitud: Double,
     val longitud: Double,
 )
 
 fun Direccion.toDTO() : DireccionDTO =
     DireccionDTO(
-        calle = this.calle,
-        altura = this.altura,
+        direccion = this.devolverDireccionCompleta(),
         latitud = this.ubicacion.x,
         longitud = this.ubicacion.y
     )
