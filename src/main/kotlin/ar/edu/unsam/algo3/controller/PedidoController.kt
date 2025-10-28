3cd64f4453baa6b1ea1c6b41d8a9b137447e04a7
carlasolrocca <carlasolrocca@gmail.com>
2025-10-28 15:27:55 -0300

    Agrego resolucion para actualizarEstado()

--- src/main/kotlin/ar/edu/unsam/algo3/controller/PedidoController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/PedidoController.kt
@@ -1,6 +1,7 @@
 package ar.edu.unsam.algo3.controller
 
 import ar.edu.unsam.algo3.Pedido
+import ar.edu.unsam.algo3.ar.edu.unsam.algo3.dto.PedidoUpdateDTO
 import ar.edu.unsam.algo3.service.PedidoService
 import ar.edu.unsam.algo3.dto.PedidoDTO
 import org.springframework.web.bind.annotation.*
@@ -10,12 +11,15 @@ import org.springframework.web.bind.annotation.*
 @CrossOrigin("*")
 class PedidoController(val pedidoService: PedidoService) {
     //Llamada redundandte porque el home ya trae los pedidos Pendientes
-    @GetMapping("/pedidos")
-    fun listarPedidos() : List<PedidoDTO> = pedidoService.getAll()
+    //@GetMapping("/pedidos")
+    //fun listarPedidos() : List<PedidoDTO> = pedidoService.getAll()
 
     @GetMapping("/pedidos", params = ["estado"])
     fun listarPedidosPorEstado(@RequestParam("estado") estado : String): List<PedidoDTO> = pedidoService.getByEstado(estado)
 
+    //Endpoint para la actualizacion de estado del Pedido
     @PatchMapping("/pedidos")
-    fun actualizarEstado() = pedidoService.actualizarEstado()   //No recibe nada porque siempre pasa al siguiente estado
+    fun actualizarEstado(@RequestBody updateDTO: PedidoUpdateDTO) {
+        pedidoService.actualizarEstado(updateDTO.id, updateDTO.nuevoEstado)
+    }
 }
