198ba9518c523381b5040f8a8c08e06da0759169
carlasolrocca <carlasolrocca@gmail.com>
2025-10-28 15:28:17 -0300

    dto para actualizar estado de pedido

---
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/PedidoUpdateDTO.kt
@@ -0,0 +1,7 @@
+package ar.edu.unsam.algo3.ar.edu.unsam.algo3.dto
+
+//Mini DTO para recibir el nuevo estado que viene cuando actualizas el pedido
+data class PedidoUpdateDTO (
+    val id: Int,
+    val nuevoEstado: String
+)
