8e1caf80a22880f309aeb46143fe5dcd49bf38d7
carlasolrocca <carlasolrocca@gmail.com>
2025-11-18 10:40:23 -0300

    Agrego registro para Controller

--- src/main/kotlin/ar/edu/unsam/algo3/controller/AuthUserController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/AuthUserController.kt
@@ -1,5 +1,8 @@
 package ar.edu.unsam.algo3.controller
 
+import ar.edu.unsam.algo3.dto.AuthResponse
+import ar.edu.unsam.algo3.dto.AuthResponseUsuario
+import ar.edu.unsam.algo3.dto.RegisterRequest
 import ar.edu.unsam.algo3.service.AuthUsuarioService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -8,4 +11,18 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin("*")
-class AuthUserController(private val authUserService : AuthUsuarioService) {}
+class AuthUserController(private val authUserService : AuthUsuarioService) {
+    @PostMapping("/api/registro")
+    fun registro(@RequestBody dataRegistro : RegisterRequest) : ResponseEntity<AuthResponseUsuario> {
+
+        authUserService.registrarUsuario(dataRegistro)
+
+        val response = AuthResponseUsuario(
+            success = true,
+            message= "Usuario registrado con exito!",
+            usuario = dataRegistro.usuario
+        )
+
+        return ResponseEntity(response, HttpStatus.CREATED)
+    }
+}
