7bebc4799ea1f8749dde3ff48b9db3ad081cc291
carlasolrocca <carlasolrocca@gmail.com>
2025-11-17 23:26:31 -0300

    Creo servicio para login/registro usuarios

---
+++ src/main/kotlin/ar/edu/unsam/algo3/service/AuthUserService.kt
@@ -0,0 +1,13 @@
+package ar.edu.unsam.algo3.ar.edu.unsam.algo3.service
+
+import ar.edu.unsam.algo3.Usuario
+import org.springframework.stereotype.Service
+import ar.edu.unsam.algo3.repositorios.UsuarioRepositorio
+import ar.edu.unsam.algo3.utils.HashUtils
+
+@Service
+class AuthUsuarioService(private val usuarioRepositorio : UsuarioRepositorio){
+    fun existeUser(username: String): Boolean{
+        return usuarioRepositorio.search(username).isNotEmpty()
+    }
+}
