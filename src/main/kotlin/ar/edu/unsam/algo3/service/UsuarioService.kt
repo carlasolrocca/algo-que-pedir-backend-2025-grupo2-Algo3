3c5e1c3e1ec9b1a5c5bdb4b68d29e6bd3886d01c
Joaquín Navarro <joanavarro23@hotmail.com>
2025-11-22 02:38:34 -0300

    método para obtener usuario por username en service de usuario

--- src/main/kotlin/ar/edu/unsam/algo3/service/UsuarioService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/UsuarioService.kt
@@ -1,6 +1,8 @@
 package ar.edu.unsam.algo3.service
 
+import ar.edu.unsam.algo3.dto.ClienteInfoDTO
 import ar.edu.unsam.algo3.dto.LocalAPuntuarDTO
+import ar.edu.unsam.algo3.dto.toDTO
 import ar.edu.unsam.algo3.dto.toDto
 import ar.edu.unsam.algo3.repositorios.LocalRepositorio
 import ar.edu.unsam.algo3.repositorios.UsuarioRepositorio
@@ -29,5 +31,10 @@ class UsuarioService(
 
         // Llama al metodo de Usuario que valida y puntúa
         usuario.puntuarLocal(local, puntuacion)
+    }
+
+    fun obtenerPorUsername(username: String): ClienteInfoDTO {
+        val usuario = usuarioRepositorio.getByNombre(username)
+        return usuario.toDTO()
     }
 }
