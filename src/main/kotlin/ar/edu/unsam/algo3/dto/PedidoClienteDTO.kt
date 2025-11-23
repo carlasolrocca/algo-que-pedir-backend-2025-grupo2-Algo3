61914730f673c5ed2bf3ecbe440d8ab054962b24
solalcaraz <alcarazmsol@gmail.com>
2025-11-23 17:25:08 -0300

    modifico toInfoDTO y arreglo las llamadas

--- src/main/kotlin/ar/edu/unsam/algo3/dto/PedidoClienteDTO.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/dto/PedidoClienteDTO.kt
@@ -11,6 +11,7 @@ import ar.edu.unsam.algo3.dto.PlatoClienteDTO
 import ar.edu.unsam.algo3.dto.toClienteDTO
 import ar.edu.unsam.algo3.dto.toDTO
 import ar.edu.unsam.algo3.dto.toDomain
+import ar.edu.unsam.algo3.dto.toInfoDTO
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
@@ -48,7 +49,7 @@ fun Pedido.toClienteDTO(): PedidoClienteDTO {
         recargoMedioDePago = this.costoMedioDePago(),
         tarifaEntrega = this.costoDeEntrega(),
         costoTotalPedido = this.costoTotalPedido(),
-        usuario = this.cliente.toDTO()
+        usuario = this.cliente.toInfoDTO()
     )
 }
 
